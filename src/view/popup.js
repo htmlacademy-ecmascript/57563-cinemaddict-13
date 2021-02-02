0b54497f2cb169c273574c04b7ca2b12fb5373ff
Naymov Aleksey <n.a.v.ru@mail.ru>
2021-02-02 18:15:51 +0300

    2

--- src/view/popup.js
+++ src/view/popup.js
@@ -89,7 +89,7 @@ const createPopupTemplate = (data) => {
                 </section>
               </div>
 
-              <div class="form-details__bottom-container">
+              <div class="film-details__bottom-container">
                 <section class="film-details__comments-wrap">
                   <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.length}</span></h3>
 
